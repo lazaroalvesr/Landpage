509dff19ea28a6605f0905795dbc9a0b534aee9e
lazaro <lazaroalves12355@gmail.com>
2024-02-10 19:01:12 -0300

    fea: atualização de pasta

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { CiMenuFries } from "react-icons/ci";
+
+export default function Header() {
+
+  const [ativaMenu, setAtivaMenu] = useState(false);
+
+  function toggle() {
+    setAtivaMenu((ativaMenu) => !ativaMenu);
+  }
+
+  return (
+    <div className="">
+      <header className="pb-3 lg:max-w-6xl lg:m-auto mt-4 flex lg:items-center lg:justify-between ">
+        <div className="flex lg:justify-evenly items-center ">
+          <div className="flex lg:mt-9 ml-2 lg:ml-0">
+            <Link href="/" className="flex items-center lg:w-80 w-32">
+              <Image
+                src="/img/icone10.png"
+                alt="Icone Money"
+                width={100}
+                height={12}
+              />
+              <p className="pl-2 text-2xl font-semibold whitespace-nowrap">Arruda Bombas</p>
+            </Link>
+            <div className="lg:hidden absolute right-5 top-7 ">
+              <button onClick={() => toggle()} >
+                <CiMenuFries size={24} />
+              </button>
+            </div>
+          </div>
+          <div className="lg:ml-96 -ml-4 pl-4 items-center pt-6 relative">
+            <ul className={`lg:flex gap-4 lg:static lg:h-auto ${ativaMenu ? 'absolute bg-white -ml-32 left-0 w-[398px] text-center top-20 h-96 pt-32 -mt-4 flex-col z-50' : "h-0 overflow-hidden"}`}>
+              <li className="text-3xl lg:text-sm text-gray-900 font-semibold">
+                <Link href={'/'}>
+                  Home
+                </Link>
+              </li>
+              <li className="text-3xl lg:text-sm text-gray-900 font-semibold">
+                <Link href={'/dasboard'}>
+                  Sobre Nós
+                </Link>
+              </li>
+              <li className="text-3xl lg:text-sm text-gray-900  font-semibold">
+                <Link href={'/'}>
+                  Nossos Serviços
+                </Link>
+              </li>
+              <li className="text-3xl lg:text-sm text-gray-900 font-semibold">
+                <Link href={'/'}>
+                  Fale Conosco
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
---
+++ src/app/components/Main.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image'
+import { ImCheckboxChecked } from "react-icons/im";
+
+
+function Main() {
+  return (
+    <div>
+      <div className='relative mb-12'>
+        <div className='absolute bg-gray-900/60 w-full h-[590px] lg:h-[700px]'>
+          <div className='flex lg:flex-row flex-col m-auto justify-center gap-44 lg:ml-32'>
+            <div className='max-w-4xl lg:mt-32 mt-8 ml-3'>
+              <div className='lg:w-[510px]'>
+                <h1 className='text-gray-50 lg:text-5xl text-2xl'><span className='text-blue-400'>Bomba de Concreto de Alta Performance</span>: Elevando o Padrão da sua Construção!</h1>
+                <p className='text-gray-50 lg:pt-8 pt-12 pb-4 lg:pb-0 w-80 lg:w-[470px]'>A transformação começa aqui! Com nossa Bomba de Concreto, da base ao acabamento, asseguramos a excelência para uma construção segura e duradoura.</p>
+              </div>
+              <button className='h-14 p-3 bg-blue-500 rounded-md mt-9 hover:scale-105 transition-transform'>
+                <p className='text-gray-50 flex ml-2 lg:text-2xl'>
+                  <span className='pr-2 mt-1'><ImCheckboxChecked color='#00ffff' /></span>
+                  Adquira já a sua bomba de concreto.</p>
+              </button>
+            </div>
+            <div className='max-w-4xl lg:mt-56 -mt-32'>
+              <ul className='lg:ml-0 ml-4'>
+                <li className='flex text-gray-50 text-xl pb-2'>
+                  <span className='pr-2 mt-1'><ImCheckboxChecked color='#00ffff' /></span>
+                  Garantia de qualidade
+                </li>
+                <li className='flex text-gray-50 text-xl pb-2'>
+                  <span className='pr-2 mt-1'><ImCheckboxChecked color='#00ffff' /></span>
+                  Clientes satisfeitos
+                </li>
+
+                <li className='flex text-gray-50 text-xl lg:w-96'>
+                  <span className='pr-2 mt-1'><ImCheckboxChecked color='#00ffff' /></span>
+                  Somos Especialistas na construção de Bomba De concreto usinado
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='w-full h-screen '>
+          <Image
+            src="/img/img_banner.png"
+            alt='Imagem pessoas espalhando concreto no chão'
+            width={900}
+            height={400}
+            className='w-full h-full object-cover'
+          />
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Main
