7034f068213aaf63a084598294ddd738765667d1
lazaro <lazaroalves12355@gmail.com>
2024-02-12 17:26:38 -0300

    ajustes v6.1

--- src/app/components/DuvidaEmpresa.tsx
+++ src/app/components/DuvidaEmpresa.tsx
@@ -4,7 +4,7 @@ function DuvidaEmpresa() {
   return (
     <div className='mt-32'>
       <div className='text-center'>
-        <h1 className='text-3xl'>Em Duvida com a quanto a nossa Empresa</h1>
+        <h1 className='text-3xl'>Em dúvida quanto a nossa empresa</h1>
         <p className='pt-5 text-xl'>Nosso Clientes</p>
       </div>
     </div>
