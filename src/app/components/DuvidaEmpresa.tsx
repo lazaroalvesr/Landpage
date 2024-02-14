6a76e55f5df3497d43cb4090a550702fe1765302
lazaro <lazaroalves12355@gmail.com>
2024-02-14 20:41:42 -0300

    ajustes

--- src/app/components/DuvidaEmpresa.tsx
+++ src/app/components/DuvidaEmpresa.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
 import Image from 'next/image';
 
 
 function DuvidaEmpresa() {
-
   return (
     <div className='mt-32'>
       <div className='text-center'>
         <h1 className='text-3xl'>Em dúvida quanto à nossa empresa</h1>
         <p className='pt-5 text-xl'>Nossos Clientes</p>
         <div className='flex lg:flex-row flex-col m-auto items-center justify-center gap-12 mt-20 lg:mt-32'>
-          <div>
+          <div className='lg:w-56'>
             <Image
               src="/img/Clientes/Cliente_01.jpg"
               alt='Cliente'
@@ -19,7 +17,7 @@ function DuvidaEmpresa() {
             />
           </div>
 
-          <div>
+          <div className='lg:w-56'>
             <Image
               src="/img/Clientes/Cliente_02.jpg"
               alt='Cliente'
@@ -28,7 +26,7 @@ function DuvidaEmpresa() {
             />
           </div>
 
-          <div>
+          <div className='lg:w-56'>
             <Image
               src="/img/Clientes/Cliente_03.jpg"
               alt='Cliente'
