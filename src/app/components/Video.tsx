8271b91662ac8569d5e86f06d82d8b32f3b1e30a
lazaro <lazaroalves12355@gmail.com>
2024-02-16 19:12:23 -0300

    ajustes reponsividade

--- src/app/components/Video.tsx
+++ src/app/components/Video.tsx
@@ -1,9 +1,8 @@
 export function Video() {
   return (
-    <div className='max-w-4xl m-auto lg:mt-52 -mt-14 flex lg:flex-row  flex-col'>
-
-      <div className=" flex lg:flex-row flex-col md:flex-row mt-32 lg:mt-0 ml-24 md:ml-0 justify-center lg:ml-0 md:gap-20 gap-20">
-        <div className="lg:w-[280px] w-56 md:w-52">
+    <div className='max-w-4xl m-auto lg:mt-52 -mt-14 flex lg:flex-row    flex-col'>
+      <div className=" flex lg:flex-row flex-col md:flex-row mt-32 lg:mt-0 md:-ml-2 ml-[70px] lg:ml-64  md:gap-16 gap-20">
+        <div className="lg:w-[280px] w-56 md:w-52 ">
           <video width="440" height="240" playsInline controls >
             <source src="/img/video1.mp4" type="video/mp4" />
             <track
